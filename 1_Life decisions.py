571d4efc2742232993e3f51349907353aeaf7724
Josh Dadak <Bigtalljosh@users.noreply.github.com>
2016-05-10 13:00:30 +0100

    Added Files

---
+++ 1_Life decisions.py
@@ -0,0 +1,5 @@
+#Life decisions
+import random
+
+decisions=["Stay in bed","Go for a walk","Go clothes shopping","Hang out with friends","Do some programming!"]
+print("What to do today: ",random.choice(decisions))
